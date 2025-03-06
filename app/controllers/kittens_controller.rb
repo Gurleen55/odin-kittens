b975243083bfd2f7232955344d507f35e9f2adb8
Gurleen55 <gurleencan6@gmail.com>
2025-03-06 12:18:09 -0400

    crud app implementation complete

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -1,2 +1,52 @@
 class KittensController < ApplicationController
+  def index
+    @kittens = Kitten.all
+  end
+
+  def show
+    @kitten = Kitten.find(params[:id])
+  end
+
+  def new
+    @kitten = Kitten.new
+  end
+
+  def create
+    @kitten = Kitten.new(kitten_params)
+
+    if @kitten.save
+      redirect_to root_path, notice: "Kitten Added Successfully"
+    else
+      flash[:alert] = "Failed to add Kitten. Please check the errors below."
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @kitten = Kitten.find(params[:id])
+  end
+
+  def update
+    @kitten = Kitten.find(params[:id])
+
+    if @kitten.update(kitten_params)
+      redirect_to root_path, notice: "Kitten Updated Successfully"
+    else
+      flash[:alert] = "Failed to update Kitten. Please check the errors below."
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @kitten = Kitten.find(params[:id])
+    @kitten.destroy
+    flash[:notice] = "Kitten Deleted Successfully"
+    redirect_to root_path
+  end
+
+  private
+
+  def kitten_params
+    params.expect(kitten: [ :name, :age, :cuteness, :softness ])
+  end
 end
